d92e3ade9f8d16332be60bc6dc9aa66fdf55266a
민서 김 <dg4res@gmail.com>
2021-09-08 16:01:01 +0900

    vector 배웠다!

--- 20210908_vector_ex2/소스.cpp
+++ 20210908_vector_ex2/소스.cpp
@@ -4,14 +4,17 @@ using namespace std;
 
 int main() {
 	vector<int> v;
-	cout << "현재의 v = ";
 	for (int i = 0; i < 10; i++) {
 		v.push_back(i);
-		cout << v[i] << ' ';
+	}
+	cout << "현재의 v = ";
+	for (auto& e : v) {
+		cout << e << ' ';
 	}
 	cout << "\n삭제 요소 = ";
 	while (!v.empty()) {
 		cout << v.back() << ' ';
 		v.pop_back();
 	}
+	cout << '\n';
 }
--- 20210908_vector_ex32/main.cpp
+++ 20210908_vector_ex32/main.cpp
@@ -3,6 +3,7 @@
 #include <ctime>
 #include <windows.h>
 #include <conio.h>
+#include <vector>
 using namespace std;
 
 class Circle {
@@ -10,6 +11,7 @@ public:
 	int x, y;
 	int radius;
 	Circle() : x{ 0 }, y{ 0 }, radius{ 0 }{};
+	Circle(int x, int y, int radius) : x{ x }, y{ y }, radius{ radius }{};
 	void print() {
 		cout << "¹ÝÁö¸§: " << radius << " @(" << x << ", " << y << ")\n";
 	}
@@ -21,26 +23,14 @@ public:
 };
 
 int main() {
+	vector<Circle> objArray;
 	srand(time(NULL));
-	Circle objArray[10];
-	char ah;
-	while (true) {
-		ah = _getch();
-		if (ah == 'q') {
-			break;
-		}
-		if (ah != 'c') {
-			cout << "ERROR";
-			break;
-		}
-		for (Circle& c : objArray) {
-			c.x = rand() % 500;
-			c.y = rand() % 300;
-			c.radius = rand() % 100;
-		}
-		for (Circle c : objArray) {
-			c.draw();
-		}
+	for (int i = 0; i < 10; i++) {
+		Circle obj{ rand() % 300, rand() % 300, rand() % 100 };
+		objArray.push_back(obj);
+	}
+	for (Circle c : objArray) {
+		c.print();
 	}
 	return 0;
 }
