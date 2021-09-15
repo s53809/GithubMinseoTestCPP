7abfadf4a2a19bad9e6d1b9a59097eaf0d03bc5e
민서 김 <dg4res@gmail.com>
2021-09-15 16:11:12 +0900

    sort사용

--- 20210915_vector_person_sort/main.cpp
+++ 20210915_vector_person_sort/main.cpp
@@ -10,7 +10,7 @@ bool compare(Person& p, Person& q) {
 
 int main() {
 	vector<Person> list;
-	list.push_back(Person("강감찬", "1234561234567", 75, 1, 160));	
+	list.push_back(Person("강감찬", "1234561234567", 75, 1, 160));
 	list.push_back(Person("이순신", "1234561234567", 65, 1, 170));
 	list.push_back(Person("홍길동", "1234561234567", 55, 1, 180));
 	for (int i = 0; i < list.size(); i++) {
